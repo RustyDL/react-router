d8474f1ea55c83b538d17a17e39e0330daafb592
RustyDL <jason.m.garcia04@gmail.com>
2024-01-17 20:09:42 -0500

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
+import Yellow from "./components/Yellow";
 
-function App() {
+export default function App() {
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <h1>React Router Colors</h1>
+      <div id="navbar">
+        <Link to="/home">Home</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/yellow">Yellow</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/yellow" element={<Yellow />} />
+        </Routes>
+      </div>
+      <div>
+        <footer className="footer">
+          <Link to="/home">Home</Link>
+          <Link to="/red">Red</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/yellow">Yellow</Link>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
